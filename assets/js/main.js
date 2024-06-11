a7cee414a070ae5ac77cfb1be550a72c939ad61c
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2024-06-11 17:26:56 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,7 +229,7 @@ fetch('https://raw.githubusercontent.com/Alplox/teles/92d8b002d10933c6d54631acff
   .catch(error => console.error('Error fetching data:', error));
 
 // MARK: Fetch secundario (lista m3u)
-fetch('https://raw.githubusercontent.com/Navezjt/NAVEZ-SP/main/spa.m3u')
+fetch('https://raw.githubusercontent.com/Navezjt/NAVEZ-SP/main/spa-movies-kids.m3u')
   .then(response => response.text())
   .then(data => {
     let parseM3u = M3U_A_JSON(data);
